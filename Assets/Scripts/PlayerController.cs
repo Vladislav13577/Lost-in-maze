27fa6c54c643e3cadb5dd549e2ccc3f198cd061f
Oleg Golubev <traicer95@gmail.com>
2024-11-20 14:44:31 +0300

    fixed camera controller, add game over scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerController : MonoBehaviour
@@ -6,44 +7,58 @@ public class PlayerController : MonoBehaviour
     private float speed = 4.5f;
     private float verticalInput;
     private float horizontalInput;
-    private float rotateSpeed = 100;
     public Transform mainCamera;
     private float mouseYinput;
     private float mouseXinput;
+    public float sensetivity = 1;
     public bool stopGame;
     public AudioClip gameOverSound;
     public AudioSource audioSource;
-    private GameManager gameManager;
+    //private GameObject gameOverScreen;
+    private float camVertRot = 0;
 
     void Start()
     {
+        //gameOverScreen = GameObject.Find("GameOverScreen");
+        //gameOverScreen.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        mouseXinput = Input.GetAxis("Mouse X");
-        mouseYinput = Input.GetAxis("Mouse Y");
-
+        mouseXinput = Input.GetAxis("Mouse X") * sensetivity;
+        mouseYinput = Input.GetAxis("Mouse Y") * sensetivity;
+        camVertRot -= mouseYinput;
+        camVertRot = Mathf.Clamp(camVertRot, -90, 90);
         if (!stopGame)
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
             transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
-            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime * mouseXinput);
-            mainCamera.Rotate(Vector3.left * rotateSpeed * Time.deltaTime * mouseYinput);
+            transform.Rotate(Vector3.up * mouseXinput);
+            mainCamera.localEulerAngles = Vector3.right * camVertRot;
         }
     }
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Anomaly"))
         {
-            gameManager.GameOver();
+            GameOver();
             stopGame = true;
+            AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
+            for (int i = 0; i < audioSources.Length; i++) 
+                audioSources[i].Stop();
+            audioSource.Stop();
             audioSource.PlayOneShot(gameOverSound);
         }
+    }
+    public void GameOver()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        //gameOverScreen.SetActive(true);
+        SceneManager.LoadScene("GameOver");
     }
 }
